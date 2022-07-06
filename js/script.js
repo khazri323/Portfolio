046166b9368fbd174b5fa457a6ca53254d838e69
khazri525 <khazrinoura065@gmail.com>
2022-07-06 19:40:56 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+const menu = document.getElementById("menu");
+const actions = document.getElementById("actions");
+
+
+menu.addEventListener("click" , () => {
+       hundleMenu();
+});
+
+function hundleMenu(){
+   menu.classList.toggle("is-active");
+   actions.classList.toggle("is-active");
+}
+
+
+
+
+
